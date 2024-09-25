130f6edfef8cba5ab3fc86100ca74f97a0e68fe3
daniel benitez <134009360+Samuel-hongo@users.noreply.github.com>
2024-09-24 22:36:17 -0500

    Update proyecto_libreria.sql

--- proyecto_libreria.sql
+++ proyecto_libreria.sql
@@ -1,7 +1,9 @@
-CREATE TABLE "sqlite_sequence" (
-	"name"	,
-	"seq"	
-);
+-- crear base de datos
+CREATE DATABASE libreria;
+
+USE libreria;
+
+-- crear tabla libro
 CREATE TABLE "libro" (
 	"ID_libro"	INTEGER NOT NULL UNIQUE,
 	"titulo"	TEXT NOT NULL,
@@ -9,12 +11,16 @@ CREATE TABLE "libro" (
 	"genero"	TEXT NOT NULL,
 	PRIMARY KEY("ID_libro" AUTOINCREMENT)
 );
+
+-- crear tabla detalle_venta
 CREATE TABLE "detalle_venta" (
 	"ID_detalle_venta"	INTEGER NOT NULL UNIQUE,
 	"cantidad"	INTEGER NOT NULL,
 	"precio_unidad"	INTEGER NOT NULL,
 	PRIMARY KEY("ID_detalle_venta" AUTOINCREMENT)
 );
+
+-- crear tabla editorial
 CREATE TABLE "editorial" (
 	"ID_editorial"	INTEGER NOT NULL UNIQUE,
 	"nombre"	TEXT NOT NULL,
@@ -23,6 +29,8 @@ CREATE TABLE "editorial" (
 	PRIMARY KEY("ID_editorial" AUTOINCREMENT),
 	FOREIGN KEY("ID_libro") REFERENCES "libro"("ID_libro")
 );
+
+crear tabla usuario
 CREATE TABLE "usuario" (
 	"ID_usuario"	INTEGER NOT NULL UNIQUE,
 	"nombre"	TEXT NOT NULL,
@@ -30,6 +38,8 @@ CREATE TABLE "usuario" (
 	"telefono"	INTEGER UNIQUE,
 	PRIMARY KEY("ID_usuario" AUTOINCREMENT)
 );
+
+-- crear tabla venta
 CREATE TABLE "venta" (
 	"ID_venta"	INTEGER NOT NULL UNIQUE,
 	"fecha"	TEXT NOT NULL,
